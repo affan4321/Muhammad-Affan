0e3b113e1af9f7714542f7a2fc8d0d3f5ef016de
Muhammad Affan <68880293+affan4321@users.noreply.github.com>
2026-05-31 18:37:21 +0500

    update

--- dark-tunnel-portfolio/src/components/TunnelShell.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-// Temporarily disable the tunnel shell (walls/ceiling) to allow debugging
-// and tuning of camera, cart and other scene elements without occlusion.
-const TunnelShell = () => null;
-
-export default TunnelShell;
